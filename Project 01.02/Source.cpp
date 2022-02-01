1f699be7850c7099fbfbdabe2921fe0598eba5df
Dmytro <=>
2022-02-01 15:01:05 +0200

    add int class car_dealership set end print

--- Project 01.02/Source.cpp
+++ Project 01.02/Source.cpp
@@ -33,6 +33,16 @@ public:
         cin >> price;
     }
 
+    void print() {
+        string s(50, '-');
+        cout << s;
+        cout << "\nCar name : " << name << "\n";
+        cout << "Year of manufacture of the car : " << year << "\n";
+        cout << "Engine capacity : " << eng_capacity << "\n";
+        cout << "Price : " << price << "\n";
+        cout << s;
+    }
+
 
 };
 
@@ -40,10 +50,30 @@ class car_dealership {
     vector<car> cars;
 public:
 
+    void set() {
+        int size;
+        car c;
+        cout << "Enter count of machines : ";
+        cin >> size;
+        for (int i = 0; i < size; i++) {
+            c.set();
+            cars.push_back(c);
+        }
+    }
+
+    void print() {
+        for (int i = 0; i < cars.size(); i++) {
+            cars[i].print();
+        }
+    }
+
+
+
 };
 int main() {
     setlocale(LC_ALL, "UKR");
-
+    
+    
 
     return 0;
 }
